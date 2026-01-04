5c7a60f7759b979e1a2b28cb10b5500e1a79d9be
Rajmund09 <prabhushankarmund@gmail.com>
2026-01-05 00:34:04 +0530

    Create main.py
    it helps to place cursor at its optimize place as per user screen size.

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyautogui
+while True:
+    a = pyautogui.position()
+    print(a)
+# click to open whatsapp
+# 910,1168
+# drag
+# 694,212 to 1161,1114
+# send bar to click
+# 1013,1093
+# send button
+# 1873,1089
+
